0227250593d3dfb765a31f40629e1e6927dc7de7
mayingbing <mayingbing@163.com>
2016-05-04 11:21:15 +0800

    text

--- text.php
+++ text.php
@@ -1,44 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=GBK">
+    <title>HTML Upload File</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype = "multipart/form-data">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000"/>
+    choose the file need be uploaded:<input type="file" id="upfile" name="upfile"/><br>
+    <input type="submit" value="upload" id = "submit"/>
+</form>
+</body>
+</html>
+upload.php:
 <?php
-
-    class Animal
+$dirfile ='C:\\zhulei\\Rubbish\\PHP Program\\PHPUpload\\';
+if(!isset($_FILES['upfile']))
+{//如果没有提交过来，就提示没有文件，从新上传
+    echo "no file,please <a href = 'HTML.php'> upload </a> again!";
+}
+else
+{
+    $fileImg = $_FILES['upfile'];
+    if(!in_array($fileImg['type'], array('image/jpeg','image/jpg','image/png')))
+    {//如果文件不是png或jpg 图片，提示必须限定这两种
+        echo "just can upload jpg and png picture,please <a href = 'HTML.php'> upload </a> again!";
+    }
+    else
     {
-        public $name = "Animal name";
-        public $age = 18;
-        public function cry()
+        if(!move_uploaded_file($fileImg['tmp_name'], $dirfile.$fileImg['name']))
+        {//如果从临时文件拷贝到我们的目标路径失败，就提示错误
+            echo "move file error, please <a href = 'HTML.php'> upload </a> again!";
+        }
+        else
         {
-            echo "Animal can cry";
-            echo "<br>";
+            echo "upload successful!";//如果都ok,就提示上传成功
         }
     }
-
-    class cat extends Animal
-    {
-        public $name;
-        public $color;
-        function __construct($selfname,$color,$parentname,$age)
-        {
-            $this->name = $selfname;
-            $this->color = $color;
-            $this->age=$age;
-        }
-        public function cry()
-        {
-            echo "cat can cry";
-            echo "<br>";
-        }
-        public function printout()
-        {
-            self::cry();
-            parent::cry();
-        }
-    }
-    $cat = new cat("cat name","red","animalname",19);
-    $cat->printout();
-    echo "this.\$name is $cat->name";
-    echo "<br>";
-    echo "this.\$age is $cat->age";
-    echo "<br>";
-    echo "this.\$color is $cat->color";
-    echo "<br>";
-
+}
 ?>
+
